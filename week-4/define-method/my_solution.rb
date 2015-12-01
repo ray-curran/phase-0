297462cc7a6e15cbd459a7593fdd612020c9785c
ray-curran <rpcurr@gmail.com>
2015-12-01 12:16:11 -0600

    Add Variables and Methods content

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,8 @@
+# Define an Empty Method
+def do_something(me,myself,i)
+end
+# I worked on this challenge [by myself, with: ].
+# by myself
+
+# Your Solution Below
+do_something("ray",27,"curran")
