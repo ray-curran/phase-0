9fd46d0dc00e4315a42114673e010e4cbb8d9275
ray-curran <rpcurr@gmail.com>
2015-12-28 16:34:09 -0600

    Add reflection to file

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -4,58 +4,56 @@
 # We spent [#] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# The require_relative statement checks that the file listed is available and can use data from it. 
+# This differs from require because it can use a relative path rather than an absolute path.
 require_relative 'state_data'
 
 class VirusPredictor
-
+#declare instance variables to be used through the class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
-
+#combines results of following methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
-  private
-
-  def predicted_deaths(population_density, population, state)
+  private #prevents calling from outside of the class's methods
+#prints number of deaths in a given state based on population density
+  def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      prediction = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      prediction = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      prediction = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      prediction = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      prediction = 0.05
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    print "#{@state} will lose #{(prediction * @population).floor} people in this outbreak"
 
   end
-
-  def speed_of_spread(population_density, state) #in months
+#calculates speed of spread based on population density
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
+    speed = 2.5
+    
     if @population_density >= 200
-      speed += 0.5
+      speed -= 2
     elsif @population_density >= 150
-      speed += 1
+      speed -= 1.5
     elsif @population_density >= 100
-      speed += 1.5
+      speed -= 1
     elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+      speed -= 0.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -82,6 +80,55 @@ california.virus_effects
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
 
-
+STATE_DATA.each do |state, info|
+  state = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
+  state.virus_effects
+end
 #=======================================================================
 # Reflection Section
+
+=begin
+
+- What are the differences between the two different hash syntaxes shown in the state_data file?
+  One uses the rocket symbol and one uses a colon. The one with the colon automatically turns the keys into symbols.
+
+- What does require_relative do? How is it different from require?
+  Require relative makes sure the file listed is there. It is different from require because it allows you to 
+  put a relative path. 
+
+- What are some ways to iterate through a hash?
+  My favorite is the each method, but you could also use a while loop or map, collect, etc. 
+
+- When refactoring virus_effects, what stood out to you about the variables, if anything?
+  That they were unnecessary. This took us a while to figure out. 
+
+- What concept did you most solidify in this challenge?
+  It was good to work through hashes again in Ruby now that we've jumped into javascript. I'm going to need
+  to work harder to make sure I keep them both straight. 
+
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
